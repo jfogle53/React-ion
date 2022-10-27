990bf2009d67645053d434b77f0978a371c9cde6
James Fogle <jfogle53@gmail.com>
2022-10-27 09:27:45 -0400

    Deploying my react app to github pages

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import profileImage from './img/Chicago skyline of myself.jpg'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> James' Personal Website </h1>
+      <h2>Author: James Fogle</h2>
+      <img src={profileImage} alt="Profile Picture"></img>
+    </div>
+  );
+}
+
+export default App;
